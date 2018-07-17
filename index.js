178781c67152260629cc361c241c426e358eaf7a
MustardPlus <mustardplusminus@gmail.com>
2018-07-17 12:18:02 -0700

    Add files via upload
    added new tagline, "Watch yourself..."

--- index.js
+++ index.js
@@ -22,13 +22,14 @@ function set_img(id, path){
 }
 
 function generate_tagline(){
-	var s_id = Math.floor(Math.random()*4);
+	var s_id = Math.floor(Math.random()*5);
 	switch(s_id){
 		case 1: str = "There's a reason coming..."; break;
 		case 2: str = "Don't look down..."; break;
 		case 3: str = "There's only one way to be..."; break;
-		default:
 		case 4: str = "What's decided will happen..."; break;
+		default:
+		case 5: str = "Watch yourself..."; break;
 	}
 	set_text("txt_tagline", str);
 }
